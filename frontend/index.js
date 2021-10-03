c0a0b5743a0d2f797a36ea21767b236ce43c031d
Profe DT <dtheriano@gmail.com>
2021-10-03 18:35:33 -0300

    Verifica metamask instalado

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,12 @@
 function init(){
+    const metakStatusDiv = document.getElementById('metaStatusDivId');
+    const metakStatusText = document.getElementById('metaStatusTextId');
+    const metaButton = document.getElementById('metaButtonId');
     if(typeof window.ethereum !== 'undefined'){
         console.log("Metamask/Web3 instalado");
+        metakStatusDiv.className = "badge bg-success text-wrap";
+        metakStatusText.innerHTML = "Metamask/Web3 disponible!!!";
+        metaButton.classList.remove('d-none');
     } else{
         console.log("Instala Metamask/Web3!!!");
     }
